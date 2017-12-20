233649f5b9971871440fed2a286b9fe64b911e21
xtine <christine@fieryorange.com>
2017-12-19 22:44:26 -0700

    comments and refactoring

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
       // set current time
       $('.now').html(timeNow.format('h:mm a'));
 
-      // run through the bus timetable
+      // loop through the bus timetable
       $.each(timetable[destination], function (i, time) {
         var busTime = moment(time, 'H:mm');
         var formattedBusTime = busTime.format('h:mm a');
@@ -19,40 +19,33 @@
         if (busTime.isAfter(timeNow)) {
           $destinationClass.find('.time').html(formattedBusTime);
 
-          var timeLeft;
+          // show hours or minutes until next bus
           var minutesLeft = moment.duration(busTime.diff(timeNow)).asMinutes();
+          var hoursLeft = moment.duration(busTime.diff(timeNow)).asHours();
+          var timeLeft;
 
-          // over an hour wait
-          if (minutesLeft > 59) {
+          // show minutes until bus arrives
+          if (parseInt(minutesLeft) < 59) {
             timeLeft = "In ";
-            timeLeft += Math.abs(parseInt(
-              moment.duration(busTime.diff(timeNow)).asHours()
-            ));
-            timeLeft += " hours";
+            timeLeft += Math.abs(parseInt(minutesLeft));
+
+            if (parseInt(minutesLeft) > 1) {
+              timeLeft += " minutes";
+            }
+            else {
+              timeLeft += " minute";
+            }
           }
-          // show minutes left
+          // if over an hour wait (early mornings)
           else {
-            timeLeft = "In ";
-            timeLeft += Math.abs(parseInt(
-              moment.duration(busTime.diff(timeNow)).asMinutes()
-            ));
-            timeLeft += " minutes";
+            timeLeft = "In over ";
+            timeLeft += Math.abs(parseInt(hoursLeft));
 
-            $destinationClass.find('.next-buses ul').empty();
-
-            // show next three buses
-            for (b = 1; b < 4; b++) {
-              var nextBusTime = timetable[destination][i + b];
-              if (nextBusTime) {
-                $destinationClass.find('.next-buses ul')
-                .append('<li>')
-                .append(moment(nextBusTime, 'H:mm').format('h:mm a'));
-              }
-              else if (!nextBusTime && b === 1) {
-                $destinationClass.find('.next-buses ul')
-                .append('<li>')
-                .append('No more for today.');
-              }
+            if (hoursLeft < 2) {
+              timeLeft += " hour";
+            }
+            else {
+              timeLeft += " hours";
             }
           }
 
@@ -61,6 +54,28 @@
           }
           else {
             $destinationClass.find('.time-left').html('Leaving now!');
+          }
+
+          // clear any previous next bus info
+          $destinationClass.find('.next-buses ul').empty();
+
+          // show next three buses
+          for (b = 1; b < 4; b++) {
+            // get next bus
+            var nextBusTime = timetable[destination][i + b];
+
+            // if there is a bus, append to next bus info
+            if (nextBusTime) {
+              $destinationClass.find('.next-buses ul')
+              .append('<li>')
+              .append(moment(nextBusTime, 'H:mm').format('h:mm a'));
+            }
+            // if there are no more buses
+            else if (!nextBusTime && b === 1) {
+              $destinationClass.find('.next-buses ul')
+              .append('<li>')
+              .append('No more for today.');
+            }
           }
 
           // set flag for incoming bus
@@ -76,10 +91,11 @@
         );
         $destinationClass.find('.time-left').html('Next bus leaves tomorrow morning');
       }
-
-      $destinationClass.on('click', function () {
-        $(this).find('.next-buses').show();
-      });
+      else {
+        $destinationClass.on('click', function () {
+          $(this).find('.next-buses').show();
+        });
+      }
     };
 
     var getBusTimes = function () {
